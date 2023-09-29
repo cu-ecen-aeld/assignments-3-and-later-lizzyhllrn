5c9f98f1a22920d51ce88994413df0a388725b54
lizsPC <lizzyhllrn@gmail.com>
2023-09-29 10:48:33 -0600

    updated fider-test again

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -13,7 +13,7 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat ${SCRIPT_DIR}/etc/finder-app/conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -37,7 +37,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then

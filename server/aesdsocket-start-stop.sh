790d22e7d68e6a2d5e8965d0e791b22800af1d3f
lizsPC <lizzyhllrn@gmail.com>
2023-11-22 19:44:09 -0700

    modified start -stop script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,11 +3,11 @@
 case "$1" in 
  start)
    echo "Starting aesdsocket"
-   start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+   start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket-simple -- -d
    ;;
  stop)
    echo "Stopping aesdsocket"
-   start-stop-daemon -K -n /usr/bin/aesdsocket
+   start-stop-daemon -K -n /usr/bin/aesdsocket-simple
    ;;
   *)
    echo "Usage: $0 {start|stop}"
